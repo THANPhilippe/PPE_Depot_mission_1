cfe2b2963719f08c46737e5cf69cd06b32726430
phili <phili@10.20.129.92>
2016-09-21 12:05:01 +0200

    1er commit phil

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
 include("vues/v_entete.php") ;
+echo "salu" ;
 session_start();
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
